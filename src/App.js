6718bcf188d3a10093e2119b014fafb1172db492
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2023-08-27 00:19:20 -0400

    Implemented the modal to confirm or cancel the clear cart action.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CartContainer from './components/CartContainer';
 import Navbar from './components/Navbar';
 import { useDispatch, useSelector } from 'react-redux';
 import { calculateTotals } from './redux/cart/cartSlice';
+import Modal from './components/Modal';
 
 function App() {
   const { cartItems } = useSelector((state) => state.cart);
@@ -14,6 +15,7 @@ function App() {
 
   return (
       <main>
+        <Modal />
         <Navbar />
         <CartContainer />
       </main>

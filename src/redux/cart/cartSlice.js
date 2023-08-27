0625e5f0b5dc37f7c4566f19a02e39c7e314790c
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2023-08-27 00:01:09 -0400

    Implemented the reducer to calculate the total of the items.

--- src/redux/cart/cartSlice.js
+++ src/redux/cart/cartSlice.js
@@ -3,7 +3,7 @@ import cartItems from '../../cartItems';
 
 const initialState = {
   cartItems: cartItems,
-  amount: 4,
+  amount: 0,
   total: 0,
   isLoading: true,
 };
@@ -26,10 +26,20 @@ const cartSlice = createSlice({
     decrease: (state, { payload }) => {
       const cartItem = state.cartItems.find((item) => item.id === payload.id);
       cartItem.amount = cartItem.amount - 1;
+    },
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = total.toFixed(2);
     }
   }
 });
 
-export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease, calculateTotals } = cartSlice.actions;
 
 export default cartSlice.reducer;

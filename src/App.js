bb26a6f939e8a8e007b94367bf715d66edd26beb
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2023-09-03 21:49:29 -0400

    Fetch data from an API with Redux-Toolkit using createAsyncThunk.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import { useEffect } from 'react';
 import CartContainer from './components/CartContainer';
 import Navbar from './components/Navbar';
 import { useDispatch, useSelector } from 'react-redux';
-import { calculateTotals } from './redux/cart/cartSlice';
+import { calculateTotals, getCartItems } from './redux/cart/cartSlice';
 import Modal from './components/Modal';
 
 function App() {
-  const { cartItems } = useSelector((state) => state.cart);
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
   const { isOpen } = useSelector((state) => state.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems, dispatch]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if(isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
       <main>
